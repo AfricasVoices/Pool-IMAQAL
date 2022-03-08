1e54cc55af5150758a7f2cf13347e1260146db45
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-03-08 18:14:26 +0300

    remove imaqal flow configs

--- configurations/create_imaqal_database.py
+++ configurations/create_imaqal_database.py
@@ -26,18 +26,6 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
             ),
             sync_config=RapidProToEngagementDBConfiguration(
                 flow_result_configurations=[
-                    FlowResultConfiguration("imaqal_demog", "age", "age"),
-                    FlowResultConfiguration("imaqal_demog", "district", "location"),
-                    FlowResultConfiguration("imaqal_demog", "gender", "gender"),
-                    FlowResultConfiguration("imaqal_demog", "household_language", "household_language"),
-                    FlowResultConfiguration("imaqal_demog", "recently_displaced", "recently_displaced"),
-
-                    FlowResultConfiguration("imaqal_covid19_demog", "age", "age"),
-                    FlowResultConfiguration("imaqal_covid19_demog", "district", "location"),
-                    FlowResultConfiguration("imaqal_covid19_demog", "gender", "gender"),
-                    FlowResultConfiguration("imaqal_covid19_demog", "household_language", "household_language"),
-                    FlowResultConfiguration("imaqal_covid19_demog", "recently_displaced", "recently_displaced"),
-
                     FlowResultConfiguration("ssf_elections_demog", "age", "age"),
                     FlowResultConfiguration("ssf_elections_demog", "district", "location"),
                     FlowResultConfiguration("ssf_elections_demog", "gender", "gender"),
