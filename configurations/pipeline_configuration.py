68de62accbf6a3406b6522268b7ea850b488ddaa
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-03-15 13:47:27 +0300

    update coda dataset config

--- configurations/pipeline_configuration.py
+++ configurations/pipeline_configuration.py
@@ -47,7 +47,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                                                     somali.DemographicCleaner.clean_age_within_range(text))
                                                 ),
                     ],
-                    ws_code_string_value="age"
+                    ws_code_match_value="age"
                 ),
                 CodaDatasetConfiguration(
                     coda_dataset_id="IMAQAL_gender",
@@ -56,7 +56,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                         CodeSchemeConfiguration(code_scheme=load_code_scheme("gender"),
                                                 auto_coder=somali.DemographicCleaner.clean_gender)
                     ],
-                    ws_code_string_value="gender"
+                    ws_code_match_value="gender"
                 ),
                 CodaDatasetConfiguration(
                     coda_dataset_id="IMAQAL_household_language",
@@ -64,7 +64,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                     code_scheme_configurations=[
                         CodeSchemeConfiguration(code_scheme=load_code_scheme("household_language"), auto_coder=None)
                     ],
-                    ws_code_string_value="hh language"
+                    ws_code_match_value="household_language"
                 ),
                 CodaDatasetConfiguration(
                     coda_dataset_id="IMAQAL_location",
@@ -79,7 +79,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                         CodeSchemeConfiguration(code_scheme=load_code_scheme("somalia_state"), auto_coder=None),
                         CodeSchemeConfiguration(code_scheme=load_code_scheme("somalia_zone"), auto_coder=None),
                     ],
-                    ws_code_string_value="location"
+                    ws_code_match_value="location"
                 ),
                 CodaDatasetConfiguration(
                     coda_dataset_id="IMAQAL_recently_displaced",
@@ -88,7 +88,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                         CodeSchemeConfiguration(code_scheme=load_code_scheme("recently_displaced"),
                                                 auto_coder=somali.DemographicCleaner.clean_yes_no)
                     ],
-                    ws_code_string_value="recently displaced"
+                    ws_code_match_value="recently_displaced"
                 ),
             ],
             ws_correct_dataset_code_scheme=load_code_scheme("ws_correct_dataset"),
