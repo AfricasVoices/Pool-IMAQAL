790d097dbf076ffe5a075af5bb72e8392f6c4203
Daniel Mwendwa <danielmwendwam@gmail.com>
2026-03-30 12:50:16 +0300

    update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -40,6 +40,10 @@ echo "Starting a new pipeline run with id ${RUN_ID}"
     --incremental-cache-volume "$PIPELINE_NAME-google-forms-to-engagement-db-cache" \
     "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
 
+./docker-run-kobotoolbox-to-engagement-db.sh \
+    --incremental-cache-volume "$PIPELINE_NAME-kobotoolbox-to-engagement-db-cache"  \
+    "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
+
 ./docker-sync-engagement-db-to-coda.sh \
     --incremental-cache-volume "$PIPELINE_NAME-engagement-db-to-coda-cache" \
     "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
